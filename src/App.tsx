b550b3246227424d7a398f7209ce44ce7737577a
Vinicius Mendite <vinicius.miff@gmail.com>
2021-03-22 15:22:03 -0300

    create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { GlobalStyle } from "./styles/global";
+import { Header } from "./components/Header";
 
 export  function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
