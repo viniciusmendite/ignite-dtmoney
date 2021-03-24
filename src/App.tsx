5098dc745c3719a9db9ef96b8b5f3aa1305100a1
Vinicius Mendite <vinicius.miff@gmail.com>
2021-03-24 10:02:22 -0300

    create summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { GlobalStyle } from "./styles/global";
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
 
 export  function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
