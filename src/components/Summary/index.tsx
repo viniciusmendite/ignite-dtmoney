6596387a622e9bc9c8531551cce53931122273f8
Vinicius Mendite <vinicius.miff@gmail.com>
2021-03-25 16:02:24 -0300

    calc resume

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -8,6 +8,22 @@ import { Container } from "./styles";
 
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+
+    return acc;
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0
+  })
   
   return (
     <Container>
@@ -16,21 +32,36 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas"/>
         </header>
-        <strong>R$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.deposits)}  
+        </strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Entradas"/>
         </header>
-        <strong>- R$500,00</strong>
+        <strong>-
+        {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.withdraws)} 
+        </strong>
       </div>
       <div>
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Entradas"/>
         </header>
-        <strong> R$500,00</strong>
+        <strong>
+        {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.total)} 
+        </strong>
       </div>
     </Container>
   );
